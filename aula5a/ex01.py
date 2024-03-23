67ba72353e1332b9b0cd105f4a956bc8d859e19d
guipiva <gui_piva159@hotmail.com>
2024-03-22 21:05:36 -0300

    aula de exercicios

---
+++ aula5a/ex01.py
@@ -0,0 +1,6 @@
+x = int(input("Digite o valor de x: "))
+y = int(input("digite o valor de y: "))
+if x >> y:
+    print("O mair valor é: ",x)
+else:
+    print("o maior valor é: ",y)
---
+++ aula5a/ex02.py
@@ -0,0 +1,9 @@
+fruta = str(input("voce vai querer qual fruta ?"))
+if (fruta == "banana"):
+        print("o kilo da banana é: 5,23")
+elif (fruta == "maça"):
+        print("o kilo da maça é: 12,10")
+elif (fruta == "cereja"):
+        print("o kilo da cereja é: 58,00")
+else:
+        print("essa fruta nao temos")
